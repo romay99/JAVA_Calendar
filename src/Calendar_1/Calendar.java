2349f8a7dd5c60a01901b92215933711bf44f86d
SooBin_Lee <i_romay@naver.com>
2022-04-07 13:25:53 +0900

    re UTF

--- src/Calendar_1/Calendar.java
+++ src/Calendar_1/Calendar.java
@@ -2,7 +2,6 @@ package Calendar_1;
 
 public class Calendar {
 	public static void main(String[] args) {
-
 		System.out.println(" 일   월   화   수   목   금   토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
