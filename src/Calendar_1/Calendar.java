e6c50cc19f6822d0aa8eeb85ac527c1b4e812d5d
SooBin_Lee <i_romay@naver.com>
2022-04-07 14:32:24 +0900

    whlie 으로 반복횟수 기능 추가
    getMonthDays 메소드 추가
    
    printCalendar 메소드 추가

--- src/Calendar_1/Calendar.java
+++ src/Calendar_1/Calendar.java
@@ -3,60 +3,25 @@ package Calendar_1;
 import java.util.Scanner;
 
 public class Calendar {
+
+
 	public static void main(String[] args) {
-		System.out.println(" 일   월   화   수   목   금   토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
-		System.out.println("몇월을 알고싶으세요: ");
 		Scanner sc = new Scanner(System.in);
-		int dal = sc.nextInt();
-		sc.close();
-		int day = 0;
+		System.out.println("몇번 반복할까요?: ");
+		int loop = sc.nextInt();
+		int i = 0;
 
-		switch (dal) {
-		case 1:
-			day = 31;
-			break;
-		case 2:
-			day = 28;
-			break;
-		case 3:
-			day = 31;
-			break;
-		case 4:
-			day = 30;
-			break;
-		case 5:
-			day = 31;
-			break;
-		case 6:
-			day = 30;
-			break;
-		case 7:
-			day = 31;
-			break;
-		case 8:
-			day = 31;
-			break;
-		case 9:
-			day = 30;
-			break;
-		case 10:
-			day = 31;
-			break;
-		case 11:
-			day = 30;
-			break;
-		case 12:
-			day = 31;
-			break;
-		default:
-			System.out.println("잘못된 입력입니다.");
+		getDays get = new getDays();
+
+		while (i < loop) {
+			System.out.println("몇월을 알고싶으세요?");
+			int month = sc.nextInt();
+			System.out.printf("%d월은 %d일 까지있습니다.\n", month, get.getMonthDays(month));
+			i++;
 		}
-		System.out.printf("입력하신 %d월은 %d 일 까지입니다", dal, day);
+		
+		get.printCalendar();
+
+		sc.close();
 	}
 }
---
+++ src/Calendar_1/getDays.java
@@ -0,0 +1,60 @@
+package Calendar_1;
+
+public class getDays {
+	
+	public void printCalendar() {
+		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+
+
+	int day = 0;
+
+	int getMonthDays(int Month) {
+		switch (Month) {
+		case 1:
+			day = 31;
+			break;
+		case 2:
+			day = 28;
+			break;
+		case 3:
+			day = 31;
+			break;
+		case 4:
+			day = 30;
+			break;
+		case 5:
+			day = 31;
+			break;
+		case 6:
+			day = 30;
+			break;
+		case 7:
+			day = 31;
+			break;
+		case 8:
+			day = 31;
+			break;
+		case 9:
+			day = 30;
+			break;
+		case 10:
+			day = 31;
+			break;
+		case 11:
+			day = 30;
+			break;
+		case 12:
+			day = 31;
+			break;
+		default:
+			System.out.println("잘못된 입력입니다.");
+		}
+		return day;
+	}
+}
