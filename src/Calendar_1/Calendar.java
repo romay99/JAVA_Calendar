7eeb10cb2e0143f8caa15b66c8a0a7b915aafa02
SooBin_Lee <i_romay@naver.com>
2022-04-08 22:31:41 +0900

    여러가지 수정.

--- src/Calendar_1/Calendar.java
+++ src/Calendar_1/Calendar.java
@@ -3,26 +3,32 @@ package Calendar_1;
 import java.util.Scanner;
 
 public class Calendar {
+	private final static String PROM = "cal> ";
 
-	public static void main(String[] args) {
-		String prom = "cal> ";
+	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		getDays get = new getDays();
 		int month = 0;
 
 		while (true) {
 			System.out.println("몇월을 알고싶으세요?");
-			System.out.print(prom);
+			System.out.print(PROM);
 			month = sc.nextInt();
 			if (month == -1) {
 				break;
-			}
-			else if(month > 12){
+			} else if (month > 12) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일 까지있습니다.\n", month, get.getMonthDays(month));
+			get.printCalendar(2022, month);
 		}
 
 		sc.close();
+
+	}
+
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		cal.runPrompt();
+
 	}
 }
--- src/Calendar_1/getDays.java
+++ src/Calendar_1/getDays.java
@@ -2,13 +2,24 @@ package Calendar_1;
 
 public class getDays {
 	
-	public void printCalendar() {
+	public void printCalendar(int year,int month) {
+		System.out.printf("   <<%4d년 %3d월>>\n",year,month);
 		System.out.println(" 일   월   화   수   목   금   토");
 		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		
+		int maxDay= getMonthDays(month);
+		
+		for(int i =1 ; i<= maxDay; i++) {
+			System.out.printf("%3d",i);
+			if ( i %7 ==0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
 	}
 
 
