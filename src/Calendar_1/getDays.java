c792fb9ab352eb1c9b8954ac7088189ea7a7b108
SooBin_Lee <i_romay@naver.com>
2022-04-08 22:31:55 +0900

    여러가지 수정

--- src/Calendar_1/getDays.java
+++ src/Calendar_1/getDays.java
@@ -16,10 +16,6 @@ public class getDays {
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 	}
 
 
